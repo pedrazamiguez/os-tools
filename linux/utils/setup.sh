2c0ea616d4827fdd6e4b8e71dabfda7d3e4cc698
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2022-11-11 16:20:16 +0100

    Changed the way the linux release name is retrieved.

--- linux/utils/setup.sh
+++ linux/utils/setup.sh
@@ -8,15 +8,27 @@ install_on_debian() {
     su -c "apt update && apt install ${PACKAGES_TO_INSTALL} -y && snap install core yt-dlp"
 }
 
+install_on_fedora() {
+    sudo dnf install \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+    sudo dnf update -y
+    sudo dnf install ${PACKAGES_TO_INSTALL} -y
+    sudo ln -s /var/lib/snapd/snap /snap
+    sudo snap install core yt-dlp
+}
+
 clear
-dist=$(lsb_release -a 2>&1 | grep -w "Distributor ID" | cut -d":" -f2 | xargs)
+dist=$(cat "/etc/os-release" | grep -w "ID" | cut -d= -f2 | xargs)
 
 echo ""
 echo "Initialising..."
 echo ""
 
 case "$dist" in
-    "Debian") install_on_debian;;
+    "debian") install_on_debian;;
+    "fedora") install_on_fedora;;
     *) echo "Linux distribution could not be determined."; exit 1;;
 esac
 
